e89c5be3dec10dd369111c008a3d11890edcada9
Avinash Patnala <avinash@glooko.com>
2020-09-29 15:31:24 +0530

    fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
- namespace :api do
-  resources :user,  :except => [:index] 
-  get 'users', to: 'user#index'
-  get 'typeahead/:input', to: 'user#typeahead'
+  namespace :api do
+    resources :user,  :except => [:index] 
+    get 'users', to: 'user#index'
+    get 'typeahead/:input', to: 'user#typeahead'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
